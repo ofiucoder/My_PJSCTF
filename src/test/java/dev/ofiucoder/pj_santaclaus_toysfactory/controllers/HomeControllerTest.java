84b657c3a2efc5dad9cf98069637cc0dcd32ad9e
Alberto Casas <Alberto.Casas.ofiuco@gmail.com>
2024-11-22 17:26:32 +0100

    test: [dev] app and HomeController

---
+++ src/test/java/dev/ofiucoder/pj_santaclaus_toysfactory/controllers/HomeControllerTest.java
@@ -0,0 +1,10 @@
+package dev.ofiucoder.pj_santaclaus_toysfactory.controllers;
+
+import org.junit.jupiter.api.Test;
+
+public class HomeControllerTest {
+    @Test
+    void testIndex() {
+
+    }
+}
