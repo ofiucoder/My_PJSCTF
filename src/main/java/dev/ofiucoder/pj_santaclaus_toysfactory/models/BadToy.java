7dd8293bdf4592aa5b3bb2f817c5f0a25f7d448b
Alberto Casas <Alberto.Casas.ofiuco@gmail.com>
2024-11-22 16:34:31 +0100

    test: [test/mvc] add BadToyTest

---
+++ src/main/java/dev/ofiucoder/pj_santaclaus_toysfactory/models/BadToy.java
@@ -0,0 +1,29 @@
+package dev.ofiucoder.pj_santaclaus_toysfactory.models;
+
+public class BadToy extends Toy {
+    private String content;
+
+    public BadToy(String id, String customId, String title, String content) {
+        super(id, customId, title, 2);
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public String toString() {
+        String toyDetails = "❄️";
+        toyDetails += customId + ".\n";
+
+        String cyanColor = "\u001B[36m";
+        String resetColor = "\u001B[0m";
+
+        toyDetails += cyanColor + "Title: " + title + "\n" + resetColor;
+        toyDetails += cyanColor + "Content: " + content + "\n" + resetColor;
+
+        return toyDetails;
+    }
+
+}
---
+++ src/test/java/dev/ofiucoder/pj_santaclaus_toysfactory/models/BadToyTest.java
@@ -0,0 +1,15 @@
+package dev.ofiucoder.pj_santaclaus_toysfactory.models;
+
+import org.junit.jupiter.api.Test;
+
+public class BadToyTest {
+    @Test
+    void testGetContent() {
+
+    }
+
+    @Test
+    void testToString() {
+
+    }
+}
