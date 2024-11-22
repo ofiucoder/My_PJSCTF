9fb299abcc0e909e1ec666cbfdd3e56788720120
Alberto Casas <Alberto.Casas.ofiuco@gmail.com>
2024-11-22 16:14:12 +0100

    test: [test/mvc] add ToyTest

---
+++ src/test/java/dev/ofiucoder/pj_santaclaus_toysfactory/models/ToyTest.java
@@ -0,0 +1,30 @@
+package dev.ofiucoder.pj_santaclaus_toysfactory.models;
+
+import org.junit.jupiter.api.Test;
+
+public class ToyTest {
+    @Test
+    void testGetChildType() {
+
+    }
+
+    @Test
+    void testGetCustomId() {
+
+    }
+
+    @Test
+    void testGetId() {
+
+    }
+
+    @Test
+    void testGetTitle() {
+
+    }
+
+    @Test
+    void testToString() {
+
+    }
+}
